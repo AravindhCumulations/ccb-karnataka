7e7721d0dd85285e9f34b89c76835e3a5e2261bd
Aravindh <aravindh.s@cumulations.com>
2025-06-25 22:39:21 +0530

    otp size

--- src/components/input-otp.tsx
+++ src/components/input-otp.tsx
@@ -45,7 +45,7 @@ const InputOTPSlot = React.forwardRef<
   )}
   {...props}
 >
-  {char && <span>&bull;</span>}
+  {char && <span style={{ fontSize: '1.5rem', lineHeight: 1 }}>&bull;</span>}
   {hasFakeCaret && (
     <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
       <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
